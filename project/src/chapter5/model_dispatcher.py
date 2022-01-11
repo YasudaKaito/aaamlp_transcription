fc46f1cf756fb479b501ef941cf44ad4b6c53f06
ykit <ykit.engineer@gmail.com>
2022-01-11 18:24:27 +0900

    p.124 xgboostまで

--- project/src/chapter5/model_dispatcher.py
+++ project/src/chapter5/model_dispatcher.py
@@ -1,6 +1,8 @@
 from sklearn import linear_model, ensemble
+import xgboost as xgb
 
 models = {
     "logres": linear_model.LogisticRegression(),
     "rf": ensemble.RandomForestClassifier,
+    "xgb": xgb.XGBClassifier,
 }
