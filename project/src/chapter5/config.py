91f8b9d2a0db3f90a73f7cebe37d5db1e6c8c59f
ykit <ykit.engineer@gmail.com>
2022-01-14 12:19:43 +0900

    p.133 数値列+LE列->xgb まで

--- project/src/chapter5/config.py
+++ project/src/chapter5/config.py
@@ -4,5 +4,7 @@ TRAINING_FILE = "../../input/cat_train_folds.csv"
 CENSUS_FILE = "../../input/adult.csv"
 CENSUS_FILE_TMP = "../../input/adult_tmp.csv"
 CENSUS_FILE_FOLDS = "../../input/adult_folds.csv"
+CENSUS_FILE_FULL_FOLDS = "../../input/adult_full_folds.csv"
+
 # 使用しない列
 REMOVE_COLS = ["fnlwgt", "age", "capital.gain", "capital.loss", "hours.per.week"]
