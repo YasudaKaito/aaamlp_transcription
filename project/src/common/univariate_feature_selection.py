b94031cb4e506321fa564946fec0d1f09a9b6b29
ykit <ykit.engineer@gmail.com>
2022-01-15 18:18:46 +0900

    p.161 相関行列まで

---
+++ project/src/common/univariate_feature_selection.py
@@ -0,0 +1,15 @@
+from sklearn.feature_selection import (
+    SelectKBest,
+    SelectPercentile,
+    chi2,
+    f_classif,
+    f_regression,
+    mutual_info_classif,
+    mutual_info_regression,
+)
+
+
+class UnivariateFeatureSelection:
+    def __init__(self, n_features, problem_type, scoring) -> None:
+        # 手法
+        pass
